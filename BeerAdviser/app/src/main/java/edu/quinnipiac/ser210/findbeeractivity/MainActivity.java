4f26d824ce31eaeffc6f5cf0c59bf27e654e8b57
Humna Hanif <hhanif@quinnipiac.edu>
2022-01-26 20:31:58 -0500

    Initial commit

---
+++ BeerAdviser/app/src/main/java/edu/quinnipiac/ser210/findbeeractivity/MainActivity.java
@@ -0,0 +1,40 @@
+package edu.quinnipiac.ser210.findbeeractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Called when the user clicks the button
+    public void onClickFindBeer(View view){
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Display the selected item
+        brands.setText(beerType);
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Display the beers
+        brands.setText(brandsFormatted);
+    }
+
+}
